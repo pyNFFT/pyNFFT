b3c176ec85cb3e8b7cddcbae781b255167e5b042
Ghislain Vaillant <ghisvail@gmail.com>
2013-05-21 08:05:48 +0100

    add Solver class empty body

--- pynfft/solver.pyx
+++ pynfft/solver.pyx
@@ -13,3 +13,59 @@
 #
 # Ghislain Vaillant
 
+cdef class Solver:
+
+    def __cinit__(self):
+        pass
+
+    def __init__(self):
+        pass
+
+    def __dealloc__(self):
+        pass
+
+    cpdef before_loop(self):
+        pass
+
+    cpdef loop_one_step(self):
+        pass
+
+    def __get_w(self):
+        pass
+
+    def __set_w(self, new_w):
+        pass
+
+    w = property(__get_w, __set_w)
+
+    def __get_w_hat(self):
+        pass
+
+    def __set_w_hat(self, new_w_hat):
+        pass
+
+    w_hat = property(__get_w_hat, __set_w_hat)
+
+    def __get_f_hat_iter(self):
+        pass
+
+    def __set_f_hat_iter(self, new_f_hat_iter):
+        pass
+
+    f_hat_iter = property(__get_f_hat_iter, __set_f_hat_iter)
+
+    def __get_r_iter(self):
+        pass
+
+    def __set_r_iter(self, new_r_iter):
+        pass
+
+    r_iter = property(__get_r_iter, __set_r_iter)
+
+    def __get_p_hat_iter(self):
+        pass
+
+    def __set_p_hat_iter(self, new_p_hat_iter):
+        pass
+
+    p_hat_iter = property(__get_p_hat_iter, __set_p_hat_iter)

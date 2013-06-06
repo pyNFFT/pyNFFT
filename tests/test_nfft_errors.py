1903e885367e4b39c1ff136aed73f771f4f70812
Ghislain Vaillant <ghisvail@gmail.com>
2013-06-06 09:22:14 +0100

    pep8 clean

--- tests/test_nfft_errors.py
+++ tests/test_nfft_errors.py
@@ -26,7 +26,6 @@ class TestNFFTError(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestNFFTError, self).__init__(*args, **kwargs)
 
-
     def test_for_invalid_N(self):
         # N must be between 0 and INT_MAX
         N = -1
@@ -38,7 +37,6 @@ class TestNFFTError(unittest.TestCase):
         N = (4, numpy.iinfo(numpy.int32).max / 2)
         self.assertRaises(ValueError, lambda: NFFT(N=N, M=M))
 
-
     def test_for_invalid_M(self):
         # M_total must be between 0 and INT_MAX
         N = 32
@@ -46,7 +44,6 @@ class TestNFFTError(unittest.TestCase):
         self.assertRaises(ValueError, lambda: NFFT(N=N, M=M))
         M = numpy.iinfo(numpy.int32).max + 1
         self.assertRaises(ValueError, lambda: NFFT(N=N, M=M))
-
 
     def test_for_invalid_n(self):
         # n must be between 0 and INT_MAX
@@ -57,20 +54,21 @@ class TestNFFTError(unittest.TestCase):
         n = numpy.iinfo(numpy.int32).max + 1
         self.assertRaises(ValueError, lambda: NFFT(N=N, M=M, n=n))
 
-
     def test_for_invalid_x(self):
         N = 32
         M = 32
         x = numpy.linspace(-0.5, 0.5, M, endpoint=False)
         x = x.astype(numpy.float64)
         # array must be contigous
-        self.assertRaises(ValueError, lambda: NFFT(N=N, M=M/2, x=x[::2]))
+        self.assertRaises(ValueError,
+                          lambda: NFFT(N=N, M=M/2, x=x[::2]))
         # array must be of the right size
-        self.assertRaises(ValueError, lambda: NFFT(N=N, M=M, x=x[:M/2]))
+        self.assertRaises(ValueError,
+                          lambda: NFFT(N=N, M=M, x=x[:M/2]))
         # array must be of the right type
         x = x.astype(numpy.float32)
-        self.assertRaises(ValueError, lambda: NFFT(N=N, M=M, x=x))
-
+        self.assertRaises(ValueError,
+                          lambda: NFFT(N=N, M=M, x=x))
 
     def test_for_invalid_f(self):
         N = 32
@@ -78,13 +76,15 @@ class TestNFFTError(unittest.TestCase):
         f = numpy.arange(M)
         f = f.astype(numpy.complex128)
         # array must be contigous
-        self.assertRaises(ValueError, lambda: NFFT(N=N, M=M/2, f=f[::2]))
+        self.assertRaises(ValueError,
+                          lambda: NFFT(N=N, M=M/2, f=f[::2]))
         # array must be of the right size
-        self.assertRaises(ValueError, lambda: NFFT(N=N, M=M, f=f[:M/2]))
+        self.assertRaises(ValueError,
+                          lambda: NFFT(N=N, M=M, f=f[:M/2]))
         # array must be of the right type
         f = f.astype(numpy.complex64)
-        self.assertRaises(ValueError, lambda: NFFT(N=N, M=M, f=f))
-
+        self.assertRaises(ValueError,
+                          lambda: NFFT(N=N, M=M, f=f))
 
     def test_for_invalid_f_hat(self):
         N = 32
@@ -92,13 +92,15 @@ class TestNFFTError(unittest.TestCase):
         f_hat = numpy.arange(N)
         f_hat = f_hat.astype(numpy.complex128)
         # array must be contigous
-        self.assertRaises(ValueError, lambda: NFFT(N=N/2, M=M, f_hat=f_hat[::2]))
+        self.assertRaises(ValueError,
+                          lambda: NFFT(N=N/2, M=M, f_hat=f_hat[::2]))
         # array must be of the right size
-        self.assertRaises(ValueError, lambda: NFFT(N=N, M=M, f_hat=f_hat[:N/2]))
+        self.assertRaises(ValueError,
+                          lambda: NFFT(N=N, M=M, f_hat=f_hat[:N/2]))
         # array must be of the right type
         f_hat = f_hat.astype(numpy.complex64)
-        self.assertRaises(ValueError, lambda: NFFT(N=N, M=M, f_hat=f_hat))
-
+        self.assertRaises(ValueError,
+                          lambda: NFFT(N=N, M=M, f_hat=f_hat))
 
     def test_for_invalid_flags(self):
         N = 32
@@ -106,7 +108,8 @@ class TestNFFTError(unittest.TestCase):
         # non existing flags
         invalid_flags = ('PRE_PHI_HOT', 'PRE_FOOL_PSI', 'FG_RADIO_PSI')
         for flag in invalid_flags:
-            self.assertRaises(ValueError, lambda: NFFT(N=N, M=M, flags=(flag,)))
+            self.assertRaises(ValueError,
+                              lambda: NFFT(N=N, M=M, flags=(flag,)))
         # managed flags
         managed_flags = []
         managed_flags.append([flag for flag in nfft_flags.keys()

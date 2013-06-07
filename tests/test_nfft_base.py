0373293e16b4538564e3343bc24aa6ba373ccf7e
Ghislain Vaillant <ghisvail@gmail.com>
2013-06-07 17:16:23 +0100

    add proper test suite for the NFFT class

--- tests/test_nfft_base.py
+++ tests/test_nfft_base.py
@@ -14,44 +14,107 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-
+from __future__ import division
+import numpy as np
 import unittest
+from numpy import pi
+from numpy.testing import assert_allclose
+from pynfft.nfft import NFFT
+from pynfft.util import vrand_unit_complex, vrand_shifted_unit_double
 
 
 class Test_NFFT_init(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         super(Test_NFFT_init, self).__init__(*args, **kwargs)
+        self.N = (16, 16)
+        self.M = 96
+        self.m = 6
+        self.flags = ('PRE_PHI_HUT', 'FG_PSI', 'PRE_FG_PSI')
 
     def test_default_args(self):
-        pass
+        Nfft = NFFT(N=self.N, M=self.M)
+
+        default_m = 12
+        self.assertEqual(Nfft.m, default_m)
+
+        default_flags = ('PRE_PHI_HUT', 'PRE_PSI')
+        for each_flag in default_flags:
+            self.assertIn(each_flag, Nfft.flags)
 
     def test_user_specified_args(self):
-        pass
+        Nfft = NFFT(N=self.N, M=self.M, m=self.m,
+                    flags=self.flags)
+
+        self.assertEqual(Nfft.d, len(self.N))
+
+        for t, Nt in enumerate(self.N):
+            self.assertEqual(Nfft.N[t], Nt)
+
+        self.assertEqual(Nfft.N_total, np.prod(self.N))
+
+        self.assertEqual(Nfft.M_total, self.M)
+
+        self.assertEqual(Nfft.m, self.m)
+
+        for each_flag in self.flags:
+            self.assertIn(each_flag, Nfft.flags)
 
 
 class Test_NFFT_runtime(unittest.TestCase):
 
+    N = (32, 32)
+    M = 1280
+
+    @staticmethod
+    def compare_with_fdft(Nfft):
+        N = Nfft.N
+        f = Nfft.f
+        f_hat = Nfft.f_hat
+        k = np.mgrid[slice(N[0]), slice(N[1])]
+        k = k.reshape([2, -1]) - np.asarray(N).reshape([2, 1]) / 2
+        x = Nfft.x.reshape([-1, 2])
+        F = np.exp(-2j * pi * np.dot(x, k))
+        f_dft = np.dot(F, f_hat)
+        assert_allclose(f, f_dft, rtol=1e-3)
+
+    @staticmethod
+    def compare_with_idft(Nfft):
+        N = Nfft.N
+        f = Nfft.f
+        f_hat = Nfft.f_hat
+        k = np.mgrid[slice(N[0]), slice(N[1])]
+        k = k.reshape([2, -1]) - np.asarray(N).reshape([2, 1]) / 2
+        x = Nfft.x.reshape([-1, 2])
+        F = np.exp(-2j * pi * np.dot(x, k))
+        f_hat_dft = np.dot(np.conjugate(F).T, f)
+        assert_allclose(f_hat, f_hat_dft, rtol=1e-3)
+
     def __init__(self, *args, **kwargs):
         super(Test_NFFT_runtime, self).__init__(*args, **kwargs)
+        self.Nfft = NFFT(N=self.N, M=self.M)
+        vrand_shifted_unit_double(self.Nfft.x)
+        self.Nfft.precompute()
 
     def test_trafo(self):
-        pass
+        vrand_unit_complex(self.Nfft.f_hat)
+        self.Nfft.trafo()
+        self.compare_with_fdft(self.Nfft)
 
     def test_trafo_direct(self):
-        pass
+        vrand_unit_complex(self.Nfft.f_hat)
+        self.Nfft.trafo_direct()
+        self.compare_with_fdft(self.Nfft)
 
     def test_adjoint(self):
-        pass
+        vrand_unit_complex(self.Nfft.f)
+        self.Nfft.adjoint()
+        self.compare_with_idft(self.Nfft)
 
     def test_adjoint_direct(self):
-        pass
-
-    def reference_fdft(x, f_hat, f):
-        pass
-
-    def reference_idft(x, f, f_hat):
-        pass
+        vrand_unit_complex(self.Nfft.f)
+        self.Nfft.adjoint_direct()
+        self.compare_with_idft(self.Nfft)
 
 
 def suite():

21937a744fea2c65063a325cf997b96d60591c1c
Ghislain Vaillant <ghisvail@gmail.com>
2013-06-07 08:48:54 +0100

    pep8 fix + add long_description for PyPI

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ ext_modules = [
     ),
 ]
 
+
 def get_version():
     basedir = os.path.dirname(__file__)
     with open(os.path.join(basedir, 'pynfft/_version.py')) as f:
@@ -66,25 +67,46 @@ def get_version():
         return variables.get('VERSION')
     raise RuntimeError('No version info found.')
 
+version = get_version()
+
+long_description = '''"The NFFT is a C subroutine library for computing the
+nonequispaced discrete Fourier transform (NDFT) in one or more dimensions, of
+arbitrary input size, and of complex data."
+
+The NFFT library is licensed under GPLv2 and available at:
+    http://www-user.tu-chemnitz.de/~potts/nfft/index.php
+
+This wrapper provides a somewhat Pythonic access to some of the core NFFT
+library functionalities and is largely inspired from the pyFFTW project
+developped by Henry Gomersall (http://hgomersall.github.io/pyFFTW/).
+
+This project is still work in progress and is still considered beta quality. In
+particular, the API is not yet frozen and is likely to change as the
+development continues. Please consult the documentation and changelog for more
+information.'''
+
+classifiers = [
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
+    'Operating System :: POSIX :: Linux',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Multimedia :: Sound/Audio :: Analysis',
+],
+
 setup_args = {
     'name': 'pyNFFT',
-    'version': get_version(),
+    'version': version,
     'author': 'Ghislain Vaillant',
-    'author_email': 'ghislain.vaillant@kcl.ac.uk',
+    'author_email': 'ghisvail@gmail.com',
     'description': 'A pythonic wrapper around NFFT',
-    'url': 'https://bitbucket.org/ghisvail/pynfft',
-    'classifiers': [
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Operating System :: POSIX :: Linux',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Multimedia :: Sound/Audio :: Analysis',
-    ],
+    'long_description': long_description,
+    'url': 'https://github.com/ghisvail/pyNFFT.git',
+    'classifiers': classifiers,
     'packages': ['pynfft'],
     'ext_modules': ext_modules,
     'include_dirs': include_dirs,

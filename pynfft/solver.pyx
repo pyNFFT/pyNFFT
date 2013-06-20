97d24eff026d69198cc48be63ad620a4b092b4f2
Ghislain Vaillant <ghisvail@gmail.com>
2013-06-20 19:02:46 +0100

    add support for different floating precision via function pointers

--- pynfft/solver.pyx
+++ pynfft/solver.pyx
@@ -17,12 +17,14 @@
 
 import numpy as np
 cimport numpy as np
+from libc.stdlib cimport malloc, free
 from cnfft3 cimport (solver_init_advanced_complex, solver_before_loop_complex,
                      solver_loop_one_step_complex, solver_finalize_complex,
-                     nfft_mv_plan_complex)
+                     nfft_mv_plan_complex, solver_plan_complex)
 from cnfft3 cimport (LANDWEBER, STEEPEST_DESCENT, CGNR, CGNE,
                      NORMS_FOR_LANDWEBER, PRECOMPUTE_WEIGHT,
                      PRECOMPUTE_DAMP,)
+from nfft cimport NFFT
 
 
 # exposes flag management internals for testing
@@ -37,9 +39,146 @@ solver_flags_dict = {
     }
 solver_flags = solver_flags_dict.copy()
 
+
+cdef void *solver_init_double(void *nfft_plan, unsigned flags):
+    cdef solver_plan_complex *ths = (
+            <solver_plan_complex *> malloc(sizeof(solver_plan_complex)))
+    if ths != NULL:
+        solver_init_advanced_complex(
+                ths, <nfft_mv_plan_complex*> nfft_plan, flags)
+    return ths
+
+cdef void solver_finalize_double(void *plan):
+    cdef solver_plan_complex *ths = <solver_plan_complex *> plan
+    solver_finalize_complex(ths)
+
+cdef void solver_before_loop_double(void *plan):
+    cdef solver_plan_complex *ths = <solver_plan_complex *> plan
+    solver_before_loop_complex(ths)
+
+cdef void solver_loop_one_step_double(void *plan):
+    cdef solver_plan_complex *ths = <solver_plan_complex *> plan
+    solver_loop_one_step_complex(ths)
+
+cdef object solver_get_w_double(void *plan, np.npy_intp shape[]):
+    cdef solver_plan_complex *ths = <solver_plan_complex *> plan
+    return np.PyArray_SimpleNewFromData(1, shape, np.NPY_FLOAT64,
+            <void *>(ths.w))
+
+cdef object solver_get_w_hat_double(void *plan, np.npy_intp shape[]):
+    cdef solver_plan_complex *ths = <solver_plan_complex *> plan
+    return np.PyArray_SimpleNewFromData(1, shape, np.NPY_FLOAT64,
+            <void *>(ths.w_hat))
+
+cdef object solver_get_y_double(void *plan, np.npy_intp shape[]):
+    cdef solver_plan_complex *ths = <solver_plan_complex *> plan
+    return np.PyArray_SimpleNewFromData(1, shape, np.NPY_COMPLEX128,
+            <void *>(ths.y))
+
+cdef object solver_get_f_hat_iter_double(void *plan, np.npy_intp shape[]):
+    cdef solver_plan_complex *ths = <solver_plan_complex *> plan
+    return np.PyArray_SimpleNewFromData(1, shape, np.NPY_COMPLEX128,
+            <void *>(ths.f_hat_iter))
+
+cdef object solver_get_r_iter_double(void *plan, np.npy_intp shape[]):
+    cdef solver_plan_complex *ths = <solver_plan_complex *> plan
+    return np.PyArray_SimpleNewFromData(1, shape, np.NPY_COMPLEX128,
+            <void *>(ths.r_iter))
+
+cdef solver_generic_init solver_init_per_dtype[1]
+
+cdef solver_generic_init* _build_solver_init_list():
+    solver_init_per_dtype[0] = <solver_generic_init>&solver_init_double
+    #solver_init_per_dtype[1] = <solver_generic_init>&solver_init_single
+    #solver_init_per_dtype[2] = <solver_generic_init>&solver_init_ldouble
+
+cdef solver_generic_finalize solver_finalize_per_dtype[1]
+
+cdef solver_generic_finalize* _build_solver_finalize_list():
+    solver_finalize_per_dtype[0] = <solver_generic_finalize>&solver_finalize_double
+    #solver_finalize_per_dtype[1] = <solver_generic_finalize>&solver_finalize_single
+    #solver_finalize_per_dtype[2] = <solver_generic_finalize>&solver_finalize_ldouble
+
+cdef solver_generic_before_loop solver_before_loop_per_dtype[1]
+
+cdef solver_generic_before_loop* _build_solver_before_loop_list():
+    solver_before_loop_per_dtype[0] = <solver_generic_before_loop>&solver_before_loop_double
+    #solver_before_loop_per_dtype[1] = <solver_generic_before_loop>&solver_before_loop_single
+    #solver_before_loop_per_dtype[2] = <solver_generic_before_loop>&solver_before_loop_ldouble
+
+cdef solver_generic_loop_one_step solver_loop_one_step_per_dtype[1]
+
+cdef solver_generic_loop_one_step* _build_solver_loop_one_step_list():
+    solver_loop_one_step_per_dtype[0] = <solver_generic_loop_one_step>&solver_loop_one_step_double
+    #solver_loop_one_step_per_dtype[1] = <solver_generic_loop_one_step>&solver_loop_one_step_single
+    #solver_loop_one_step_per_dtype[2] = <solver_generic_loop_one_step>&solver_loop_one_step_ldouble
+
+cdef solver_generic_get_w solver_get_w_per_dtype[1]
+
+cdef solver_generic_get_w* _build_solver_get_w_list():
+    solver_get_w_per_dtype[0] = <solver_generic_get_w>&solver_get_w_double
+    #solver_get_w_per_dtype[1] = <solver_generic_get_w>&solver_get_w_single
+    #solver_get_w_per_dtype[2] = <solver_generic_get_w>&solver_get_w_ldouble
+
+cdef solver_generic_get_w_hat solver_get_w_hat_per_dtype[1]
+
+cdef solver_generic_get_w_hat* _build_solver_get_w_hat_list():
+    solver_get_w_hat_per_dtype[0] = <solver_generic_get_w_hat>&solver_get_w_hat_double
+    #solver_get_w_hat_per_dtype[1] = <solver_generic_get_w_hat>&solver_get_w_hat_single
+    #solver_get_w_hat_per_dtype[2] = <solver_generic_get_w_hat>&solver_get_w_hat_ldouble
+
+cdef solver_generic_get_y solver_get_y_per_dtype[1]
+
+cdef solver_generic_get_y* _build_solver_get_y_list():
+    solver_get_y_per_dtype[0] = <solver_generic_get_y>&solver_get_y_double
+    #solver_get_y_per_dtype[1] = <solver_generic_get_y>&solver_get_y_single
+    #solver_get_y_per_dtype[2] = <solver_generic_get_y>&solver_get_y_ldouble
+
+cdef solver_generic_get_f_hat_iter solver_get_f_hat_iter_per_dtype[1]
+
+cdef solver_generic_get_f_hat_iter* _build_solver_get_f_hat_iter_list():
+    solver_get_f_hat_iter_per_dtype[0] = <solver_generic_get_f_hat_iter>&solver_get_f_hat_iter_double
+    #solver_get_f_hat_iter_per_dtype[1] = <solver_generic_get_f_hat_iter>&solver_get_f_hat_iter_single
+    #solver_get_f_hat_iter_per_dtype[2] = <solver_generic_get_f_hat_iter>&solver_get_f_hat_iter_ldouble
+
+cdef solver_generic_get_r_iter solver_get_r_iter_per_dtype[1]
+
+cdef solver_generic_get_r_iter* _build_solver_get_r_iter_list():
+    solver_get_r_iter_per_dtype[0] = <solver_generic_get_r_iter>&solver_get_r_iter_double
+    #solver_get_r_iter_per_dtype[1] = <solver_generic_get_r_iter>&solver_get_r_iter_single
+    #solver_get_r_iter_per_dtype[2] = <solver_generic_get_r_iter>&solver_get_r_iter_ldouble
+
+
+cdef object solver_complex_dtypes
+solver_complex_dtypes = {
+        np.dtype('float64'): np.dtype('complex128')
+        #np.dtype('float32'): np.dtype('complex64')
+        #np.dtype('float128'): np.dtype('complex256')
+        }
+
+cdef object solver_dtype_to_index
+solver_dtype_to_index = {
+        np.dtype('float64'): 0
+        #np.dtype('float32'): 1
+        #np.dtype('float128'): 2
+        }
+
+
 # Numpy must be initialized. When using numpy from C or Cython you must
 # _always_ do that, or you will have segfaults
 np.import_array()
+
+# initialize module
+_build_solver_init_list()
+_build_solver_finalize_list()
+_build_solver_before_loop_list()
+_build_solver_loop_one_step_list()
+_build_solver_get_w_list()
+_build_solver_get_w_hat_list()
+_build_solver_get_y_list()
+_build_solver_get_f_hat_iter_list()
+_build_solver_get_r_iter_list()
+
 
 cdef class Solver:
     '''
@@ -63,7 +202,26 @@ cdef class Solver:
     '''
     def __cinit__(self, NFFT nfft_plan, flags=None):
 
-        # flags management
+        # check dtype and assign function pointers accordingly
+        dtype = nfft_plan.dtype
+        try:
+            dtype_complex = solver_complex_dtypes[dtype]
+            func_idx = solver_dtype_to_index[dtype]
+            self.__solver_init = solver_init_per_dtype[func_idx]
+            self.__solver_finalize = solver_finalize_per_dtype[func_idx]
+            self.__solver_before_loop = solver_before_loop_per_dtype[func_idx]
+            self.__solver_loop_one_step = solver_loop_one_step_per_dtype[func_idx]
+            self.__solver_get_w = solver_get_w_per_dtype[func_idx]
+            self.__solver_get_w_hat = solver_get_w_hat_per_dtype[func_idx]
+            self.__solver_get_y = solver_get_y_per_dtype[func_idx]
+            self.__solver_get_f_hat_iter = solver_get_f_hat_iter_per_dtype[func_idx]
+            self.__solver_get_r_iter = solver_get_r_iter_per_dtype[func_idx]
+        except KeyError:
+            raise ValueError('dtype %s is not supported' % dtype)
+
+        # convert tuple of litteral precomputation flags to its expected
+        # C-compatible value. Each flag is a power of 2, which allows to compute
+        # this value using BITOR operations.
         cdef unsigned int _flags = 0
         flags_used = ()
 
@@ -87,61 +245,40 @@ cdef class Solver:
                         each_flag + '\' is not a valid flag.')
 
         # initialize plan
-        try:
-            solver_init_advanced_complex(
-                <solver_plan_complex *>&self.__plan,
-                <nfft_mv_plan_complex *>&(nfft_plan.__plan),
-                _flags)
-        except:
+        self.__plan = self.__solver_init(nfft_plan.__plan, _flags)
+        if self.__plan == NULL:
             raise MemoryError
 
-        self._dtype = nfft_plan._dtype
-        self._flags = tuple(flags_used)
+        self._dtype = dtype
+        self._flags = flags_used
 
         cdef np.npy_intp shape[1]
-        cdef int _M_total = nfft_plan._M_total
-        cdef int _N_total = nfft_plan._N_total
+        cdef int M_total = nfft_plan._M_total
+        cdef int N_total = nfft_plan._N_total
 
         if 'PRECOMPUTE_WEIGHT' in flags_used:
-            shape[0] = _M_total
-            self._w = np.PyArray_SimpleNewFromData(
-                1, shape, np.NPY_FLOAT64, <void *>self.__plan.w)
+            shape[0] = M_total
+            self._w = self.__solver_get_w(self.__plan, shape)
             self._w[:] = 1  # make sure weights are initialized
         else:
             self._w = None
 
         if 'PRECOMPUTE_DAMP' in flags_used:
-            shape[0] = _N_total
-            self._w_hat = np.PyArray_SimpleNewFromData(
-                1, shape, np.NPY_FLOAT64, <void *>self.__plan.w_hat)
-            self._w_hat[:] = 1  # make sure weights are initialized
+            shape[0] = N_total
+            self._w_hat = self.__solver_get_w_hat(self.__plan, shape)
         else:
             self._w_hat = None
 
-        shape[0] = _M_total
-        self._y = np.PyArray_SimpleNewFromData(
-            1, shape, np.NPY_COMPLEX128, <void *>self.__plan.y)
+        shape[0] = M_total
+        self._y = self.__solver_get_y(self.__plan, shape)
 
-        shape[0] = _M_total
-        self._r_iter = np.PyArray_SimpleNewFromData(
-            1, shape, np.NPY_COMPLEX128, <void *>self.__plan.r_iter)
+        shape[0] = N_total
+        self._f_hat_iter = self.__solver_get_f_hat_iter(self.__plan, shape)
+        self._f_hat_iter[:] = 0  # default initial guess
 
-        shape[0] = _N_total
-        self._f_hat_iter = np.PyArray_SimpleNewFromData(
-            1, shape, np.NPY_COMPLEX128, <void *>self.__plan.f_hat_iter)
-        self._f_hat_iter[:] = 0  # initial guess
+        shape[0] = M_total
+        self._r_iter = self.__solver_get_r_iter(self.__plan, shape)
 
-        shape[0] = _N_total
-        self._z_hat_iter = np.PyArray_SimpleNewFromData(
-            1, shape, np.NPY_COMPLEX128, <void *>self.__plan.z_hat_iter)
-
-        shape[0] = _N_total
-        self._p_hat_iter = np.PyArray_SimpleNewFromData(
-            1, shape, np.NPY_COMPLEX128, <void *>self.__plan.p_hat_iter)
-
-        shape[0] = _M_total
-        self._v_iter = np.PyArray_SimpleNewFromData(
-            1, shape, np.NPY_COMPLEX128, <void *>self.__plan.v_iter)
 
     def __init__(self, nfft_plan, flags=None):
         '''
@@ -177,19 +314,23 @@ cdef class Solver:
         pass
 
     def __dealloc__(self):
-        solver_finalize_complex(&self.__plan)
+        if self.__plan != NULL:
+            self.__solver_finalize(self.__plan)
+            free(self.__plan)
 
     cpdef before_loop(self):
         '''
         Initialize solver internals.
         '''
-        solver_before_loop_complex(&self.__plan)
+        if self.__plan != NULL:
+            self.__solver_before_loop(self.__plan)
 
     cpdef loop_one_step(self):
         '''
         Perform one iteration.
         '''
-        solver_loop_one_step_complex(&self.__plan)
+        if self.__plan != NULL:
+            self.__solver_loop_one_step(self.__plan)
 
     def __get_w(self):
         '''
@@ -246,94 +387,6 @@ cdef class Solver:
         return self._r_iter
 
     r_iter = property(__get_r_iter)
-
-    def __get_z_hat_iter(self):
-        '''
-        Residual of normal equation.
-        '''
-        return self._z_hat_iter
-
-    z_hat_iter = property(__get_z_hat_iter)
-
-    def __get_p_hat_iter(self):
-        '''
-        Search direction.
-        '''
-        return self._p_hat_iter
-
-    p_hat_iter = property(__get_p_hat_iter)
-
-    def __get_v_iter(self):
-        '''
-        Residual vector update.
-        '''
-        return self._v_iter
-
-    v_iter = property(__get_v_iter)
-
-    def __get_alpha_iter(self):
-        '''
-        Step size for search direction.
-        '''
-        return self.__plan.alpha_iter
-
-    alpha_iter = property(__get_alpha_iter)
-
-    def __get_beta_iter(self):
-        '''
-        Step size for search direction.
-        '''
-        return self.__plan.beta_iter
-
-    beta_iter = property(__get_beta_iter)
-
-    def __get_dot_r_iter(self):
-        '''
-        Weighted dotproduct of r_iter.
-        '''
-        return self.__plan.dot_r_iter
-
-    dot_r_iter = property(__get_dot_r_iter)
-
-    def __get_dot_r_iter_old(self):
-        '''
-        Previous value of dot_r_iter.
-        '''
-        return self.__plan.dot_r_iter_old
-
-    dot_r_iter_old = property(__get_dot_r_iter_old)
-
-    def __get_dot_z_hat_iter(self):
-        '''
-        Weighted dotproduct of z_hat_iter.
-        '''
-        return self.__plan.dot_z_hat_iter
-
-    dot_z_hat_iter = property(__get_dot_z_hat_iter)
-
-    def __get_dot_z_hat_iter_old(self):
-        '''
-        Previous value of dot_z_hat_iter.
-        '''
-        return self.__plan.dot_z_hat_iter_old
-
-    dot_z_hat_iter_old = property(__get_dot_z_hat_iter_old)
-
-    def __get_dot_p_hat_iter(self):
-        '''
-        Weighted dotproduct of p_hat_iter.
-        '''
-        return self.__plan.dot_p_hat_iter
-
-    dot_p_hat_iter = property(__get_dot_p_hat_iter)
-
-    def __get_dot_v_iter(self):
-        '''
-        Weighted dotproduct of v_iter.
-        '''
-        return self.__plan.dot_v_iter
-
-    dot_v_iter = property(__get_dot_v_iter)
 
     def __get_dtype(self):
         '''

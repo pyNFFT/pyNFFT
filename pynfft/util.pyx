0f56e3672fd290fb6f0b52b50808f39519920bcb
Ghislain Vaillant <ghisvail@gmail.com>
2013-06-20 17:22:11 +0100

    add docstring

--- pynfft/util.pyx
+++ pynfft/util.pyx
@@ -25,21 +25,59 @@ from cnfft3util cimport *
 np.import_array()
 
 def vrand_unit_complex (object[np.complex128_t, mode='c'] x not None):
+    '''
+    Utilitary function for initializing a vector of knots to random
+    values within the range [-0.5, 0.5).
+
+    Used for testing :attr:`~pynfft.nfft.NFFT.x`.
+
+    :param x: pre-allocated array
+    :type x: ndarray <complex128>
+    '''
     nfft_vrand_unit_complex(<fftw_complex *>&x[0], x.size)
 
 def vrand_shifted_unit_double (object[np.float64_t, mode='c'] x not None):
+    '''
+    Utilitary function for initializing a vector of data to random
+    complex values within the range [0, 1).
+
+    Used for testing :attr:`~pynfft.nfft.NFFT.f` and
+    :attr:`~pynfft.nfft.NFFT.f_hat`.
+
+    :param x: pre-allocated array
+    :type x: ndarray <float64>
+    '''
     nfft_vrand_shifted_unit_double(<double *>&x[0], x.size)
 
 def voronoi_weights_1d (object[np.float64_t, mode='c'] w not None,
                         object[np.float64_t, mode='c'] x not None):
+    '''
+    Utilitary function for computing density compensation weights from 1D knots.
+
+    :param w: pre-allocated array
+    :type w: ndarray <float64>
+    :param x: ordered 1D knots
+    :type x: ndarray <float64>
+    '''
     if x.size != w.size:
         raise ValueError('Incompatible size between weights and nodes \
                          (%d, %d)'%(w.size, x.size))
     nfft_voronoi_weights_1d(<double *>&w[0], <double *>&x[0], w.size)
 
-def voronoi_weights_S2(object[np.float64_t, mode='c'] w not None,
-                       object[np.float64_t, mode='c'] x not None):
-    if x.size != 2 * w.size:
+def voronoi_weights_S2 (object[np.float64_t, mode='c'] w not None,
+                        object[np.float64_t, mode='c'] xi not None):
+    '''
+    Utilitary function for computing density compensation weights from knots
+    located on the surface of a sphere.
+
+    Useful for reconstruction of 3D radial data.
+
+    :param w: pre-allocated array
+    :type w: ndarray <float64>
+    :param xi: angular locations (2D) on the unit sphere
+    :type xi: ndarray <float64>
+    '''
+    if xi.size != 2 * w.size:
         raise ValueError('Incompatible size between weights and nodes \
-                         (%d, %d)'%(w.size, x.size))
-    nfft_voronoi_weights_S2(<double *>&w[0], <double *>&x[0], w.size)
+                         (%d, %d)'%(w.size, xi.size))
+    nfft_voronoi_weights_S2(<double *>&w[0], <double *>&xi[0], w.size)

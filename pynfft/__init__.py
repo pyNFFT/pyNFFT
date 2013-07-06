181670d46af4ff6daefd5ffb048440c3dcc4d643
Ghislain Vaillant <ghisvail@gmail.com>
2013-07-06 14:15:57 +0100

    move main component import to root module hierarchy

--- pynfft/__init__.py
+++ pynfft/__init__.py
@@ -17,7 +17,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import
+from .nfft import NFFT, Solver
 
 from ._version import VERSION
 __version__ = VERSION
-

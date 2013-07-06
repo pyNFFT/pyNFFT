fc96bf2e4425b805243f02ff7f0d02d9ec2c694d
Ghislain Vaillant <ghisvail@gmail.com>
2013-07-06 14:01:15 +0100

    merge solver component in nfft module, plus simplify the code base

--- pynfft/nfft.pyx
+++ pynfft/nfft.pyx
@@ -22,6 +22,25 @@ from libc cimport limits
 from cnfft3 cimport *
 
 
+# Initialize module
+# Numpy must be initialized. When using numpy from C or Cython you must
+# _always_ do that, or you will have segfaults
+np.import_array()
+
+# initialize FFTW threads
+fftw_init_threads()
+
+# register FFTW threads cleanup routine
+import atexit
+@atexit.register
+def _cleanup():
+    fftw_cleanup_threads()
+
+
+########
+# NFFT #
+########
+ 
 cdef object nfft_supported_flags_tuple
 nfft_supported_flags_tuple = (
     'PRE_PHI_HUT',
@@ -58,21 +77,6 @@ fftw_flags_dict = {
     'FFTW_DESTROY_INPUT':FFTW_DESTROY_INPUT,
     }
 fftw_flags = fftw_flags_dict.copy()
-
-
-# Initialize module
-# Numpy must be initialized. When using numpy from C or Cython you must
-# _always_ do that, or you will have segfaults
-np.import_array()
-
-# initialize FFTW threads
-fftw_init_threads()
-
-# register FFTW threads cleanup routine
-import atexit
-@atexit.register
-def _cleanup():
-    fftw_cleanup_threads()
 
 
 cdef class NFFT:
@@ -471,6 +475,255 @@ cdef class NFFT:
         return self._n
 
     n = property(__get_n)
+
+    def __get_dtype(self):
+        '''
+        The floating precision.
+        '''
+        return self._dtype
+
+    dtype = property(__get_dtype)
+
+    def __get_flags(self):
+        '''
+        The precomputation flags.
+        '''
+        return self._flags
+
+    flags = property(__get_flags)
+
+
+##########
+# Solver #
+##########
+
+cdef object solver_flags_dict
+solver_flags_dict = {
+    'LANDWEBER':LANDWEBER,
+    'STEEPEST_DESCENT':STEEPEST_DESCENT,
+    'CGNR':CGNR,
+    'CGNE':CGNE,
+    'NORMS_FOR_LANDWEBER':NORMS_FOR_LANDWEBER,
+    'PRECOMPUTE_WEIGHT':PRECOMPUTE_WEIGHT,
+    'PRECOMPUTE_DAMP':PRECOMPUTE_DAMP,
+    }
+solver_flags = solver_flags_dict.copy()
+
+cdef class Solver:
+    '''
+    Solver is a class for computing the adjoint NFFT iteratively. Using the
+    solver should theoretically lead to more accurate results, even with just
+    one iteration, than using :meth:`~pynfft.nfft.NFFT.adjoint` or
+    :meth:`~pynfft.nfft.NFFT.adjoint_direct`.
+
+    The instantiation requires a NFFT object used internally for the multiple
+    forward and adjoint NFFT performed. The class uses conjugate-gradient as
+    the default solver but alternative solvers can be specified.
+
+    Because the stopping conidition of the iterative computation may change
+    from one application to another, the implementation only let you carry
+    one iteration at a time with a call to
+    :meth:`~pynfft.solver.Solver.loop_one_step`. Initialization of the solver
+    is done by calling the :meth:`~pynfft.solver.Solver.before_loop` method.
+
+    The class exposes the internals of the solver through call to their
+    respective properties. They should be treated as read-only values.
+    '''
+    cdef solver_plan_complex _plan
+    cdef NFFT _nfft_plan
+    cdef object _w
+    cdef object _w_hat
+    cdef object _y
+    cdef object _f_hat_iter
+    cdef object _r_iter
+    cdef object _dtype
+    cdef object _flags
+    
+    def __cinit__(self, NFFT nfft_plan, flags=None):
+
+        # support only double / double complex NFFT
+        # TODO: if support for multiple floating precision lands in the 
+        # NFFT library, adapt this section to dynamically figure the 
+        # real and complex dtypes
+        dtype_real = np.dtype('float64')
+        dtype_complex = np.dtype('complex128')
+
+        # convert tuple of litteral precomputation flags to its expected
+        # C-compatible value. Each flag is a power of 2, which allows to compute
+        # this value using BITOR operations.
+        cdef unsigned int _flags = 0
+        flags_used = ()
+
+        # sanity checks on user specified flags if any,
+        # else use default ones:
+        if flags is not None:
+            try:
+                flags = tuple(flags)
+            except:
+                flags = (flags,)
+            finally:
+                flags_used += flags
+        else:
+            flags_used += ('CGNR',)
+
+        for each_flag in flags_used:
+            try:
+                _flags |= solver_flags_dict[each_flag]
+            except KeyError:
+                raise ValueError('Invalid flag: ' + '\'' +
+                        each_flag + '\' is not a valid flag.')
+
+        # initialize plan
+        try:
+            solver_init_advanced_complex(self._plan,
+                <mv_plan_complex*>&(nfft_plan._plan), _flags)
+        except:
+            raise MemoryError
+        
+        self._nfft_plan = nfft_plan
+
+        cdef np.npy_intp shape[1]
+        cdef int M_total = nfft_plan._M_total
+        cdef int N_total = nfft_plan._N_total
+
+        if 'PRECOMPUTE_WEIGHT' in flags_used:
+            shape[0] = M_total
+            self._w = np.PyArray_SimpleNewFromData(1, shape,
+                np.NPY_FLOAT64, <void *>(self._plan.w))
+            self._w[:] = 1  # make sure weights are initialized
+        else:
+            self._w = None
+
+        if 'PRECOMPUTE_DAMP' in flags_used:
+            shape[0] = N_total
+            self._w_hat = np.PyArray_SimpleNewFromData(1, shape,
+                np.NPY_FLOAT64, <void *>(self._plan.w_hat))
+            self._w_hat[:] = 1  # make sure weights are initialized
+        else:
+            self._w_hat = None
+
+        shape[0] = M_total
+        self._y = np.PyArray_SimpleNewFromData(1, shape,
+            np.NPY_COMPLEX128, <void *>(self._plan.y))
+
+        shape[0] = N_total
+        self._f_hat_iter = np.PyArray_SimpleNewFromData(1, shape,
+            np.NPY_COMPLEX128, <void *>(self._plan.f_hat_iter))        
+        self._f_hat_iter[:] = 0  # default initial guess
+        
+        shape[0] = M_total
+        self._r_iter = np.PyArray_SimpleNewFromData(1, shape,
+            np.NPY_COMPLEX128, <void *>(self._plan.r_iter))
+        
+        self._dtype = dtype_real
+        self._flags = flags_used
+
+
+    def __init__(self, nfft_plan, flags=None):
+        '''
+        :param plan: instance of NFFT.
+        :type plan: :class:`~pynfft.nfft.NFFT`
+        :param flags: list of instantiation flags, see below.
+        :type flags: tuple
+
+        **Instantiation flags**
+
+        +---------------------+-----------------------------------------------------------------------------+
+        | Flag                | Description                                                                 |
+        +=====================+=============================================================================+
+        | LANDWEBER           | Use Landweber (Richardson) iteration.                                       |
+        +---------------------+-----------------------------------------------------------------------------+
+        | STEEPEST_DESCENT    | Use steepest descent iteration.                                             |
+        +---------------------+-----------------------------------------------------------------------------+
+        | CGNR                | Use conjugate gradient (normal equation of the 1st kind).                   |
+        +---------------------+-----------------------------------------------------------------------------+
+        | CGNE                | Use conjugate gradient (normal equation of the 2nd kind).                   |
+        +---------------------+-----------------------------------------------------------------------------+
+        | NORMS_FOR_LANDWEBER | Use Landweber iteration to compute the residual norm.                       |
+        +---------------------+-----------------------------------------------------------------------------+
+        | PRECOMPUTE_WEIGHT   | Weight the samples, e.g. to cope with varying sampling density.             |
+        +---------------------+-----------------------------------------------------------------------------+
+        | PRECOMPUTE_DAMP     | Weight the Fourier coefficients, e.g. to favour fast decaying coefficients. |
+        +---------------------+-----------------------------------------------------------------------------+
+
+        Default value is ``flags = ('CGNR',)``.
+        '''
+        pass
+
+    def __dealloc__(self):
+        if self.__plan != NULL:
+            self.__solver_finalize(self.__plan)
+            free(self.__plan)
+
+    cpdef before_loop(self):
+        '''
+        Initialize solver internals.
+        '''
+        if self.__plan != NULL:
+            self.__solver_before_loop(self.__plan)
+
+    cpdef loop_one_step(self):
+        '''
+        Perform one iteration.
+        '''
+        if self.__plan != NULL:
+            self.__solver_loop_one_step(self.__plan)
+
+    def __get_w(self):
+        '''
+        Weighting factors.
+        '''
+        return self._w
+
+    def __set_w(self, new_w):
+        if self._w is not None:
+            self._w.ravel()[:] = new_w.ravel()[:]
+
+    w = property(__get_w, __set_w)
+
+    def __get_w_hat(self):
+        '''
+        Damping factors.
+        '''
+        return self._w_hat
+
+    def __set_w_hat(self, new_w_hat):
+        if self._w_hat is not None:
+            self._w_hat.ravel()[:] = new_w_hat.ravel()[:]
+
+    w_hat = property(__get_w_hat, __set_w_hat)
+
+    def __get_y(self):
+        '''
+        Right hand side, samples.
+        '''
+        return self._y
+
+    def __set_y(self, new_y):
+        if self._y is not None:
+            self._y.ravel()[:] = new_y.ravel()[:]
+
+    y = property(__get_y, __set_y)
+
+    def __get_f_hat_iter(self):
+        '''
+        Iterative solution.
+        '''
+        return self._f_hat_iter
+
+    def __set_f_hat_iter(self, new_f_hat_iter):
+        if self._f_hat_iter is not None:
+            self._f_hat_iter.ravel()[:] = new_f_hat_iter.ravel()[:]
+
+    f_hat_iter = property(__get_f_hat_iter, __set_f_hat_iter)
+
+    def __get_r_iter(self):
+        '''
+        Residual vector.
+        '''
+        return self._r_iter
+
+    r_iter = property(__get_r_iter)
 
     def __get_dtype(self):
         '''

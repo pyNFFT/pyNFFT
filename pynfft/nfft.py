0b7c96846f4054f78d966f3de73151b02ac95867
Holger Kohr <ho.kohr@zoho.com>
2019-06-09 23:34:43 +0200

    Add copyright info and AUTHORS file

--- pynfft/nfft.py
+++ pynfft/nfft.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2013  Ghislain Vaillant
+# Copyright PyNFFT developers and contributors
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
